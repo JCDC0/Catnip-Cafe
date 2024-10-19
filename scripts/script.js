8e5a89685c95c362bb63cb64298fae9b3bce86f9
JCDC0 <1carlodcastro@gmail.com>
2024-10-19 21:06:31 +0800

    a
    a

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// De Castro, Juan Carlo C.
+// CS - 202 
+
+// Hamburger Menu
+function hamburger() {
+    const mobileMenu = document.querySelector('.mobile-nav ul');
+    mobileMenu.classList.toggle('show');
+}
